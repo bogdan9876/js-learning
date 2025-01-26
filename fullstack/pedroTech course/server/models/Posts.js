f1e019d69a6a7ac474ac106958cd1f980b825095
bogdan9876 <bogdankryveiko@gmail.com>
2025-01-26 15:31:20 +0200

    add initial code of projects

---
+++ fullstack/pedroTech course/server/models/Posts.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataType) => {
+
+    const Posts = sequelize.define("Posts", {
+        title : {
+            type: DataType.STRING,
+            allowNull: false,
+        },
+        postText : {
+            type: DataType.STRING,
+            allowNull: false,
+        },
+        username : {
+            type: DataType.STRING,
+            allowNull: false,
+        },
+    });
+
+    return Posts
+}
