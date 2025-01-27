0e8fcc37801b5abec5b02fa47f20eb8f18944d1d
bogdan9876 <bogdankryveiko@gmail.com>
2025-01-27 03:22:27 +0200

    go to bed in half of ep4

---
+++ fullstack/pedroTech-course/client/src/pages/CreatePost.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Formik, Form, Field, ErrorMessage} from "formik"
+
+function CreatePost() {
+  return (
+    <div className="createPostPage">
+        <Formik>
+            <Form className="formContainer">
+                <label>Title: </label>
+                <Field id="inputCreatePost" name="title" placeholder="(Ex.John...)" />
+                <label>Post: </label>
+                <Field id="inputCreatePost" name="postText" placeholder="(Ex.Post...)" />
+                <label>Username: </label>
+                <Field id="inputCreatePost" name="username" placeholder="(Ex.John111...)" />
+                <button type='submit'>Create Post</button>
+            </Form>
+        </Formik>
+    </div>
+  )
+}
+
+export default CreatePost
