182ac96e4239ff34dac7e986b0a6f8f2bb03ffb9
Bogdan <bogdankryveiko@gmail.com>
2025-01-30 01:13:51 +0200

    ep7 done

--- fullstack/pedroTech-course/client/src/pages/Post.js
+++ fullstack/pedroTech-course/client/src/pages/Post.js
@@ -1,17 +1,31 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from "axios";
 
 function Post() {
 
-  let {id} = useParams()
+  let { id } = useParams()
   const [postObject, setPostObject] = useState({})
+  const [comments, setComments] = useState([])
+  const [newComment, setNewComment] = useState("")
 
   useEffect(() => {
     axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
       setPostObject(response.data)
     });
-  })
+
+    axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
+      setComments(response.data)
+    });
+  }, [])
+
+  const addComment = () => {
+    axios.post("http://localhost:3001/comments", { commentBody: newComment, PostId: id }).then((response) => {
+      const commentToAdd = { commentBody: newComment }
+      setComments([...comments, commentToAdd])
+      setNewComment('')
+    })
+  }
 
   return (
     <div className="postPage">
@@ -22,7 +36,20 @@ function Post() {
           <div className="footer">{postObject.username}</div>
         </div>
       </div>
-      <div className="rightSide">Comment Section</div>
+      <div className="rightSide">
+        <div className='addCommentContainer'>
+          <input type="text" placeholder='Comment...'
+            value={newComment}
+            autoComplete='off'
+            onChange={(event) => { setNewComment(event.target.value) }} />
+          <button onClick={addComment}>Add Comments</button>
+        </div>
+        <div className="listOfComments">
+          {comments.map((comment, key) => {
+            return <div key={key} className='comment'>{comment.commentBody}</div>
+          })}
+        </div>
+      </div>
     </div>
   );
 }
