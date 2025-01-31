1b399066c2190b42efa632134be3870960e337a6
bogdan9876 <bogdankryveiko@gmail.com>
2025-01-31 17:27:01 +0200

    rp12

--- fullstack/pedroTech-course/server/routes/Likes.js
+++ fullstack/pedroTech-course/server/routes/Likes.js
@@ -5,17 +5,20 @@ const { validateToken } = require("../middlewares/AuthMiddleware")
 
 router.post("/", validateToken, async (req, res) => {
     const { PostId } = req.body;
-    const UserId = req.body.id;
+    const UserId = req.user.id;
 
     const found = await Likes.findOne({
-        where: { PostId: PostId, UserId: UserId}
+        where: { PostId: PostId, UserId: UserId }
     })
     if (!found) {
-    await Likes.create({PostId: PostId, UserId: UserId})
+        await Likes.create({ PostId: PostId, UserId: UserId })
+        res.json({liked: true})
     } else {
-        
+        await Likes.destroy({
+            where: { PostId: PostId, UserId: UserId },
+        })
+        res.json({liked: false})
     }
-    res.json("Success")
 })
 
 module.exports = router
